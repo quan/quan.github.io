9e13f5edabfcd3d656eca2f449fdac80a3499359
quan <tranmqt@gmail.com>
2017-06-29 19:27:00 -0700

    set itemization link to gihub

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const links = [
   {
     'title': 'Inkwell Marketing Website',
-    'url': 'https://quan.github.io/inkwell_website'
+    'url': 'https://quan.github.io/inkwell_website/'
   }, {
-    'title': 'Facebook vs. Fake News',
-    'url': 'https://quan.github.io/facebook_fakenews/main.pdf'
+    'title': 'Persona 5 Itemization Lookup',
+    'url': 'http://quan.github.io/persona5_itemization/'
   }, {
     'title': 'PolyRents',
     'url': 'https://polyrents.com'
   }, {
-    'title': 'Persona 5 Itemization Lookup',
-    'url': 'http://persona5-itemization.herokuapp.com/'
+    'title': 'AsterRide',
+    'url': 'https://www.asterride.com'
+  }, {
+    'title': 'Facebook vs. Fake News',
+    'url': 'https://quan.github.io/facebook_fakenews/main.pdf'
   }
 ]
 
