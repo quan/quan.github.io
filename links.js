11ecccc3ce7c9c1c781509859b990a056a788381
quan <tranmqt@gmail.com>
2017-06-30 19:41:02 -0700

    reordered and renamed links

---
+++ links.js
@@ -0,0 +1,27 @@
+const links = [
+  {
+    'title': 'Persona 5 Itemization',
+    'url': 'http://quan.github.io/persona5_itemization/'
+  }, {
+    'title': 'Inkwell',
+    'url': 'https://quan.github.io/inkwell_website/'
+  }, {
+    'title': 'PolyRents',
+    'url': 'https://polyrents.com'
+  }, {
+    'title': 'AsterRide',
+    'url': 'https://www.asterride.com'
+  }, {
+    'title': 'Facebook vs. Fake News',
+    'url': 'https://quan.github.io/facebook_fakenews/main.pdf'
+  }
+]
+
+window.onload = () => {
+  const projectList = document.getElementById('project-list')
+
+  for (let i in links) {
+    const link = '<li><a href=' + links[i].url + '>' + links[i].title + '</a></li>'
+    projectList.innerHTML += link
+  }
+}
