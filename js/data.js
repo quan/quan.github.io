2ba9b71ae059fb7e1ae5896c6a7b4ff103434b5c
quan <tranmqt@gmail.com>
2017-08-06 21:42:32 -0700

    hid profile picture column until picture available.

--- js/data.js
+++ js/data.js
@@ -3,10 +3,7 @@ const PROJECTS = [
     'title': 'Persona 5 Items',
     'url': 'https://quan.github.io/persona5-items/',
     'image': 'assets/images/persona5-items.png',
-    'description': {
-      'short': 'A tabular Persona 5 item database.',
-      'long': ''
-    },
+    'description': '',
     'tech-stack': [
       'React',
       'Javascript'
@@ -15,55 +12,37 @@ const PROJECTS = [
     'title': 'Madotsuki Walker',
     'url': 'https://quan.github.io/madotsuki/',
     'image': 'assets/images/madotsuki-walk.png',
-    'description': {
-      'short': 'Take a walk with Madotsuki.',
-      'long': ''
-    },
+    'description': 'Take a walk with Madotsuki.',
     'tech-stack': ''
   }, {
     'title': 'Inkwell',
     'url': 'https://quan.github.io/inkwell-website/',
     'image': 'assets/images/inkwell.png',
-    'description': {
-      'short': 'A cross-platform draw and chat app.',
-      'long': ''
-    },
+    'description': 'A cross-platform draw and chat app.',
     'tech-stack': ''
   }, {
     'title': 'PolyRents',
     'url': 'https://polyrents.com',
     'image': 'assets/images/polyrents.png',
-    'description': {
-      'short': 'Renting, simplified.',
-      'long': ''
-    },
+    'description': 'Renting, simplified.',
     'tech-stack': ''
   }, {
     'title': 'Cru',
     'url': null,
     'image': 'assets/images/cru.png',
-    'description': {
-      'short': 'A mobile app for Cru Central Coast.',
-      'long': ''
-    },
+    'description': 'A mobile app for Cru Central Coast.',
     'tech-stack': ''
   }, {
     'title': 'slamrater.com',
     'url': null,
     'image': 'assets/images/slamrater.png',
-    'description': {
-      'short': 'Support the slams that support us!',
-      'long': ''
-    },
+    'description': 'Support the slams that support us!',
     'tech-stack': ''
   }, {
     'title': 'Facebook vs. Fake News',
     'url': 'https://quan.github.io/facebook-fakenews/main.pdf',
     'image': 'assets/images/facebook-vs-fake-news.png',
-    'description': {
-      'short': 'A paper on fake news on Facebook.',
-      'long': ''
-    },
+    'description': 'A paper on fake news on Facebook.',
     'tech-stack': ''
   }
 ]
--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
    */
   function showProjectDetails (project) {
     console.log('yes: ' + project.url)
+    window.location = project.url
   }
 
   /**
@@ -15,62 +16,6 @@
     console.log('no')
   }
 
-  /**
-   * Render the given project as a jQuery and return it.
-   * @param {object} project - The project to render.
-   * @return {jQuery} A compiled jQuery element for the card.
-   */
-  function renderCard (project) {
-    const title = project.title
-    const description = project.description.short
-    const image = project.image
-    const url = project.url
-
-    // Create the title that appears within the card block.
-    const $title = $('<h5>', {
-      'class': 'card-title project-card-title',
-      'text': title
-    })
-
-    // Create the description that appears within the card block.
-    const $description = $('<p>', {
-      'class': 'card-text',
-      'text': description
-    })
-
-    // Create the card block that wraps the title and description.
-    const $block = $('<div>', {
-      'class': 'card-block'
-    }).append($title)
-      .append($description)
-
-    // Create the image that appears at the top of the card.
-    const $image = $('<img>', {
-      'class': 'card-img-bottom project-card-img',
-      'src': image,
-      'alt': title
-    }).click(function () {
-
-      showProjectDetails(project)
-    })
-    // toggle(function () {
-    //     showProjectDetails(project)
-    //   }, function () {
-    //     hideProjectDetails(project)
-    //   })
-    // }
-
-    // Compile the pieces of the card.
-    const $card = $('<div>', {
-      'class': 'card project-card'
-    }).append($block)
-      .append($image)
-
-    // Wrap the card and return it.
-    return $('<div>', {
-      'class': 'project-card-wrapper col-xl-4 col-sm-6'
-    }).append($card)
-  }
 
   /**
    * Sets the height of each of the si
@@ -87,23 +32,13 @@
   /**
    * Main. Initializes the project list and image resize listeners.
    */
-  function main () {
-    // Load and render each project into the page.
-    const projects = PROJECTS
-    const $projectContainer = $('div.project-list')
-    for (var index in projects) {
-      const project = projects[index]
-      $projectContainer.append(renderCard(project))
-    }
-
-    // Resize images and set image listeners.
-    var projectCardImages = $('img.project-card-img')
+  $(function () {
+    // Resize square images and set resize listeners.
+    const projectCardImages = $('img.card-img')
     resizeSquare(projectCardImages)
     $(window).resize(function () {
       resizeSquare(projectCardImages)
     })
-  }
-
-  $(function () { main() })
+  })
 })()
 
